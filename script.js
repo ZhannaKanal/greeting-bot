f1d4f9e22d3f771b7451a730a5c0b35ece3f3380
Zhanna <kezhanna.03@mail.ru>
2025-12-13 23:49:14 +0500

    task 8

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-console.log("Hi there!")
-console.log("I am excited to talk to you.")
+console.log("Hi there!");
+console.log("I am excited to talk to you.");
 let bot;
-bot = "teacherBot"
-let botLocation = "the universe"
-console.log("Allow me to introduce myself.")
-let botIntroduction = "My name is "+bot+".";
-console.log(botIntroduction)
+bot = "teacherBot";
+let botLocation = "the universe";
+console.log("Allow me to introduce myself.");
+let botIntroduction = "My name is " + bot + ".";
+console.log(botIntroduction);
+let botLocationSentence = "I live in " + botLocation + ".";
+console.log(botLocationSentence);
