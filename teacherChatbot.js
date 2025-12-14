e9317f795e1447bbb8f76e9cb4427a459f2a273c
Zhanna <kezhanna.03@mail.ru>
2025-12-14 23:07:11 +0500

    task 5

--- teacherChatbot.js
+++ teacherChatbot.js
@@ -4,3 +4,5 @@ const greeting = `My name is ${botName}.`;
 console.log(greeting);
 const subject = "JavaScript";
 const topic = "strings";
+const sentence = `Today, you will learn about ${topic} in ${subject}.`
+console.log(sentence);
