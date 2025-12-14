351f19c1ab1e7f73d96e4dc17cc65d4efc8045f1
Zhanna <kezhanna.03@mail.ru>
2025-12-15 01:23:49 +0500

    task 4

--- stringInspector.js
+++ stringInspector.js
@@ -1,3 +1,6 @@
 let fccSentence = "freeCodeCamp is a great place to learn web development.";
 console.log("Here are some examples of the includes() method:");
 const hasFreeCodeCamp = fccSentence.includes("freeCodeCamp");
+console.log(
+  `fccSentence.includes("freeCodeCamp") returns ${hasFreeCodeCamp} because the word "freeCodeCamp" is in the sentence.`
+);
