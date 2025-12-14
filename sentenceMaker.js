094061094cfe884f2fba8360a43efedb10a20db1
Zhanna <kezhanna.03@mail.ru>
2025-12-14 18:04:38 +0500

    task 2

--- sentenceMaker.js
+++ sentenceMaker.js
@@ -4,3 +4,19 @@ let verb = "chill";
 let place = "beach";
 let adjective2 = "near";
 let noun2 = "plane";
+let firstStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
