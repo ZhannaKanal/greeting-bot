cde4c3541d7d2cdfbb4a0f0019e258ab3103065d
Zhanna <kezhanna.03@mail.ru>
2025-12-14 23:08:35 +0500

    task 6

--- teacherChatbot.js
+++ teacherChatbot.js
@@ -4,5 +4,7 @@ const greeting = `My name is ${botName}.`;
 console.log(greeting);
 const subject = "JavaScript";
 const topic = "strings";
-const sentence = `Today, you will learn about ${topic} in ${subject}.`
+const sentence = `Today, you will learn about ${topic} in ${subject}.`;
 console.log(sentence);
+const strLengthIntro = `Here is an example of using the length property on the word ${subject}.`;
+console.log(strLengthIntro);
