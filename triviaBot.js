72746cd519b3c04e7fe34a4467d57b7e22331e2e
Zhanna <kezhanna.03@mail.ru>
2025-12-14 23:17:31 +0500

    task 9

--- triviaBot.js
+++ triviaBot.js
@@ -12,4 +12,4 @@ codingFact = "I liked " + favoriteLanguage;
 console.log(codingFact);
 codingFact = "I would like love " + favoriteLanguage;
 console.log(codingFact);
-console.log("It was fun sharing these facts with you. Goodbye! - "+botName+" from "+botLocation+".");
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
