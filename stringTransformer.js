a2d9a5b287cb6de350192f15a34876d71e12ea91
Zhanna <kezhanna.03@mail.ru>
2025-12-17 23:41:48 +0500

    task 4

--- stringTransformer.js
+++ stringTransformer.js
@@ -2,3 +2,5 @@ const originalString = "I love cats."
 console.log("Original string:");
 console.log(originalString);
 const replacedString = originalString.replace("cats", "dogs");
+console.log("After using the replace() method:")
+console.log(replacedString)
