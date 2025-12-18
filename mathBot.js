32b37369a1330d4d3c5a51493d46bfc2c2123c17
Zhanna <kezhanna.03@mail.ru>
2025-12-18 23:42:23 +0500

    task 7

--- mathBot.js
+++ mathBot.js
@@ -12,3 +12,5 @@ const max = 100;
 
 const randomNum2 = Math.random()* (max-min);
 console.log(randomNum2)
+
+console.log("The Math.floor() method rounds the value down to the nearest whole integer.")
