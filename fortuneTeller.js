5c1cf3d89ecd30b57946ddc587709421e430aed6
Zhanna <kezhanna.03@mail.ru>
2025-12-19 00:18:41 +0500

    task 6

--- fortuneTeller.js
+++ fortuneTeller.js
@@ -3,3 +3,5 @@ const fortune2 = "The weather will be nice tomorrow."
 const fortune3 = "Be cautious of your new neighbors."
 const fortune4 = "You will find a new hobby soon."
 const fortune5 = "It would be wise to avoid the color red today."
+
+const randomNumber = (Math.random()*5) + 1;
