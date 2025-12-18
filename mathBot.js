bc8b8eefd90753394a848c238ee631dba1195bac
Zhanna <kezhanna.03@mail.ru>
2025-12-18 23:58:44 +0500

    task 14

--- mathBot.js
+++ mathBot.js
@@ -26,3 +26,7 @@ const numRoundedUp = Math.ceil(3.2);
 console.log(numRoundedUp)
 
 console.log("The Math.round() method rounds the value to the nearest whole integer.")
+const numRounded = Math.round(2.7)
+const numRounded2 = Math.round(11.2)
+console.log(numRounded)
+console.log(numRounded2)
