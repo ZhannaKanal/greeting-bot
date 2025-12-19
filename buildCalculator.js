0db651ff297a9c6a5a2432d5f93873c798d49204
Zhanna <kezhanna.03@mail.ru>
2025-12-20 00:02:58 +0500

    task 20

--- buildCalculator.js
+++ buildCalculator.js
@@ -21,4 +21,5 @@ console.log(calculateProduct(13, 5))
 const calculateQuotient = (num1, num2) => {
   return num1 / num2;
 }
-console.log(calculateQuotient(7,11))
+console.log(calculateQuotient(7, 11))
+console.log(calculateQuotient(3, 0));
