d3482e247f1c362f7a5d63fd9be042f4602b4087
Zhanna <kezhanna.03@mail.ru>
2025-12-19 23:58:27 +0500

    task 14

--- buildCalculator.js
+++ buildCalculator.js
@@ -11,4 +11,8 @@ const calculateDifference = (num1, num2)=>{
 }
 console.log(calculateDifference(22,5))
 console.log(calculateDifference(12,1))
-console.log(calculateDifference(17,9))
+console.log(calculateDifference(17, 9))
+
+const calculateProduct = (num1, num2) => {
+  
+}
