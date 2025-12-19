52b5c9c595c833337e7b7488cfb575bdfe10943a
Zhanna <kezhanna.03@mail.ru>
2025-12-19 23:56:15 +0500

    task 10

--- buildCalculator.js
+++ buildCalculator.js
@@ -4,4 +4,8 @@ const calculateSum = (num1, num2) => {
 
 console.log(calculateSum(2,5));
 console.log(calculateSum(10,10))
-console.log(calculateSum(5,5))
+console.log(calculateSum(5, 5))
+
+const calculateDifference = (num1, num2)=>{
+  
+}
