0ca18ec1bc3baa39759e47370f648af93f57ddfd
Zhanna <kezhanna.03@mail.ru>
2025-12-20 14:28:41 +0500

    all tasks completed

--- emailMasker.js
+++ emailMasker.js
@@ -1,3 +1,13 @@
-const maskEmail = email => {
-  
-}
+const maskEmail = (email) => {
+  const indexOfDog = email.indexOf("@"); //12
+  const sliced = email.slice(1, indexOfDog - 1); //reecodecam
+  const repeated = "*".repeat(sliced.length); // **********
+  const masked =
+    email[0] +
+    repeated +
+    email[indexOfDog - 1] +
+    email.slice(indexOfDog, email.length); //f ********** p @example.com
+  return masked;
+};
+let email = "freecodecamp@example.com";
+console.log(maskEmail(email));
