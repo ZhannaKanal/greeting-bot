e31d1621c3b9afb15a190babab7558281f15a6c5
Zhanna <kezhanna.03@mail.ru>
2025-12-20 20:15:36 +0500

    all tasks completed

--- leapYearCounter.js
+++ leapYearCounter.js
@@ -1,3 +1,10 @@
-const isLeapYear = num => {
-  
-}
+const isLeapYear = (num) => {
+  if (num % 4 === 0 && (num % 400 === 0 || num % 100 !== 0)) {
+    return num + " is a leap year.";
+  } else {
+    return num + " is not a leap year.";
+  }
+};
+let year = 1900;
+const result = isLeapYear(year);
+console.log(result);
